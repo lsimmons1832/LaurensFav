f7022dfe4a60420eea8283852ac1e32dbf13aebb
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-12 23:40:40 -0500

    loading images to the screen

---
+++ javascripts/main.js
@@ -0,0 +1,37 @@
+var app = angular.module("Lauren", []);
+
+
+app.controller("ItemCtrl", ($scope) => {
+
+$scope.links = [
+{
+	url: "http://www.catbreedslist.com/uploads/allimg/cat-pictures/Persian-Cat-1.jpg"
+},
+{
+	url: "https://s-media-cache-ak0.pinimg.com/736x/ea/6a/7a/ea6a7ac5c2252a07f6a0434c6532842e.jpg"
+},
+{
+	url: " http://8.fotos.web.sapo.io/i/B3a138d55/17425336_fWf7n.jpeg"
+},
+{
+	url: "http://a57.foxnews.com/images.foxnews.com/content/fox-news/science/slideshow/2013/08/31/24-worlds-most-beautiful-creatures/_jcr_content/slideshow-par/slide_image1/image.img.jpg/0/549/1422618106686.jpg?ve=1&tl=1?ve=1"
+},
+{
+	url: "http://greatinspire.com/wp-content/uploads/2013/02/Beautiful-Animals-Love-Photography-15.jpg"
+},
+{
+	url: "http://2.bp.blogspot.com/-cBpudVp0a0Y/T6LneSWzDHI/AAAAAAAADJA/91fnhVFdopM/s1600/most_expensive_animals_in_the_world.jpg"
+},
+{
+	url: "http://www.beautiful-animals.com/wp-content/uploads/2012/03/Beautiful-animals-photographu-stumbleupon-5.jpg"
+},
+{
+	url: "https://www.englishforum.ch/attachments/pet-corner/62750d1368811908-most-beautiful-animal-image.jpg"
+}
+];
+
+});
+
+
+
+
